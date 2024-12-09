30025f35fe0444fd3849d5ea202fa5bf23fc873e
Tulsi Vanol <tulsivanol@gmail.com>
2024-12-09 14:47:36 -0500

    Added CI/CD

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(process.env.REACT_APP_BASE_URL)
+
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/currency",
+    baseURL: `${process.env.REACT_APP_BASE_URL}/currency`,
 });
 
 export default api;
