a4dd602fac6676712ef7776d6e6026b902b832ce
Meet Upadhyay <meet2886@gmail.com>
2024-12-08 23:44:34 -0500

    updated backend with axios

--- backend/src/routes/currencyRoutes.js
+++ backend/src/routes/currencyRoutes.js
@@ -3,6 +3,7 @@ const { convertCurrency } = require("../controllers/currencyController");
 
 const router = express.Router();
 
+// POST route for currency conversion
 router.post("/convert", convertCurrency);
 
 module.exports = router;
